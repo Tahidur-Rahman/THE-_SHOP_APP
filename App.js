22bc49d035dc727c764fa623c150bccc965e9c30
Tahidur-Rahman <mdtahidurrahman20@gmail.com>
2021-04-19 14:26:30 +0600

    The_Meal_App-----Native_Navigation

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text>The meal app</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ src/context/reducers/Provider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Provider() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Provider
